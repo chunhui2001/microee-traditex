c5df444aa1f8e11c1dcbda28ada5204f4eefe554
zhangchunhui <zhangchunhui@huobi.com>
2021-01-21 16:10:56 +0800

    a
    a

--- microee-traditex-liquid/microee-traditex-liquid-app/src/main/java/com/microee/traditex/liquid/app/App.java
+++ microee-traditex-liquid/microee-traditex-liquid-app/src/main/java/com/microee/traditex/liquid/app/App.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import com.microee.stacks.es6.config.ElasticSearchEnabled;
 import com.microee.stacks.redis.config.RedisEnabled;
 import com.microee.stacks.starter.MainApp;
 
@@ -11,6 +12,7 @@ import com.microee.stacks.starter.MainApp;
 @ComponentScan(basePackages = {"com.microee"})
 @SpringBootApplication
 @RedisEnabled()
+@ElasticSearchEnabled()
 public class App extends MainApp {
     public static void main(String[] args) throws ParseException {
         startup(App.class, args);
